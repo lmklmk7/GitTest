bc7b1e0c0a42c45ae23a0c88635b6930798986a3
lmklmk7 <1977554287@qq.com>
2017-10-25 14:40:21 +0800

    初始化一个空的c++项目

---
+++ GitText/GitText/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitText
+//
+//  Created by STUDENT on 17/10/25.
+//  Copyright © 2017年 lmk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
